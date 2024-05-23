28f32e3d98aaa1472e5cdfd137b2848db8344709
Snare-Hawk <85657152+Snare-Hawk@users.noreply.github.com>
2024-05-23 03:32:01 -0400

    this is totally wicked

--- Web/WebScraper.py
+++ Web/WebScraper.py
@@ -12,11 +12,31 @@ company = soup.find_all("h3", class_="company")
 location = soup.find_all("p", class_="location")
 date = soup.find_all("time")
 
+# def getDesc():
+description = soup.find_all("a", class_="card-footer-item", attrs={"href"})
+    
+descList = []
+
+for i in description:
+    if i.text == "Apply":
+        descList.append(i)
+
 masterList = []
+
+output = descList[0]['href']
+
+"""
+from here,
+comment code so people know how it works
+using the output string up there, be able to scrape the description and probably put it within a function
+:3
+"""
+
 
 for x, y, a, b in zip(jobTitle, company, location, date):
     info = {
         "name": str(x.text.strip()),
+        # "description": getDesc(),
         "employer": str(y.text.strip()),
         "location": str(a.text.strip()),
         "datePosted": str(b.text.strip())
